4084ef68b66e2ca5f078ae3eb9aa1fbd5d8a54b1
fransandoval093 <fransandoval093@gmail.com>
2021-09-13 20:38:25 -0700

    Hardcoded the teatroDB link into connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/teatro', {
+// mongoose.connect( "mongodb+srv://teatroDBuser:easyDBpassword@cluster0.1t258.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" || process.env.MONGODB_URI || 'mongodb://localhost/teatro', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
+
+mongoose.connect( "mongodb+srv://teatroDBuser:easyDBpassword@cluster0.1t258.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" || process.env.MONGODB_URI , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
