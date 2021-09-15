3a8c8190acd678a427b6a16a70c21015956a38c1
fransandoval093 <fransandoval093@gmail.com>
2021-09-15 06:14:39 -0700

    Server bug fixes

--- client/src/pages/MovieDetail.js
+++ client/src/pages/MovieDetail.js
@@ -12,8 +12,9 @@ function MovieDetail({ match }) {
     fetchSimilars();
     console.log(match.params.id);
   }, [match]);
-  const [movieQuery, setMovieQuery] = useState([]);
-  const [queryStatus, setQueryStatus] = useState("GET MOVIE")
+
+  const [queryResult, setQueryResult] = useState("");
+  const [queryStatus, setQueryStatus] = useState("GET MOVIE");
   const [movie, setMovie] = useState({});
   const [credits, setCredits] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
@@ -21,12 +22,10 @@ function MovieDetail({ match }) {
 
   function truncate(str, n) {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-  }
-
+  };
   const formatYear = (str, n) => {
     return str?.length > n ? str.slice(0, 4) + "" : str;
   };
-
   const formatHyphen = (punctuatedString) => {
     // var s = ", -/ is #! an $ % ^ & * example ;: {} of a = -_ string with `~)() punctuation";
     var s = punctuatedString
@@ -48,15 +47,13 @@ function MovieDetail({ match }) {
   const fetchCredits = async () => {
     const fetchCredits = await fetch(
       `
-      https://api.themoviedb.org/3/movie/${match.params.id}/credits?api_key=d42525da8940f7a7a298e98a209ec951&language=en-US`
+        https://api.themoviedb.org/3/movie/${match.params.id}/credits?api_key=d42525da8940f7a7a298e98a209ec951&language=en-US`
     );
 
     const credits = await fetchCredits.json();
     setCredits(credits.cast);
     console.log(credits);
   };
-
-  
   const fetchSimilars = async () => {
     const fetchSimilars = await fetch(
       `
@@ -67,26 +64,39 @@ function MovieDetail({ match }) {
     setSimilars(similars.results);
     console.log(similars);
   };
-
-  const fetchMovieQuery = async (movie) => {
-
+  const fetchQuery = async (movie) => {
     setQueryStatus("GETTING...")
-    const fetchList = await fetch(
+    const theFilm = await fetch(
       `/api/getList?moviename=${formatHyphen(movie.original_title)}&movieyear=${formatYear(movie.release_date, 4)}`
     );
 
-    console.log(formatHyphen(movie.original_title));
-    console.log(formatYear(movie.release_date, 4));
+    const film = await theFilm.json();
+    setQueryResult(film.slice(0, 1));
+    console.log(film);
+
+    setQueryStatus("ENJOY")
+
+    // TESTING | Various console logs
+    console.log("TESTING | Movie input\n", formatHyphen(movie.original_title));
+    console.log("TESTING | Year input\n", formatYear(movie.release_date, 4));
     console.log(movie.tagline);
 
-    const movieList = await fetchList.json();
-    const movieJson = await movieList.slice(0, 1);
+    console.log("TESTING | data \n");
+    fetch(`/api/getList?moviename=${formatHyphen(movie.original_title)}&movieyear=${formatYear(movie.release_date, 4)}`)
+      .then(response => response.json())
+      .then(data => console.log(data)
+      );
 
-    if (movieJson) {
-      setQueryStatus("GET MOVIE")
-    }
-    setMovieQuery(movieJson);
-    console.log(movieJson);
+    // JSON ERROR TESTING
+    fetch(`/api/getList?moviename=${formatHyphen(movie.original_title)}&movieyear=${formatYear(movie.release_date, 4)}`).then(async response => {
+      try {
+        const data = await response.json()
+        console.log('response data?', data)
+      } catch (error) {
+        console.log('Error happened here!')
+        console.error(error)
+      }
+    })
   };
 
   const opts = {
@@ -96,7 +106,6 @@ function MovieDetail({ match }) {
       autoplay: 1,
     },
   };
-
   // RETRIEVES TRAILER 
   const handleClick = (movie) => {
     if (trailerUrl) {
@@ -134,17 +143,19 @@ function MovieDetail({ match }) {
           </button>
           <button
             className="trailer__button movie__button"
-            onClick={() => fetchMovieQuery(movie)}>
+            onClick={() => fetchQuery(movie)}>
             {queryStatus}
           </button>
           {/* Check to see if any items are found*/}
-          {movieQuery.length ? (
+
+          {queryResult.length ? (
             <div>
               {/* Render the list of items */}
               <hr></hr>
-              {movieQuery.map((item) => {
+              {queryResult.map((item) => {
                 return (
                   <div>
+                    <hr></hr>
                     <h6>{movie.tagline}</h6>
                     <iframe title="hi"
                       name="watch" id="IframeEmbed" height="auto" width="auto"
@@ -160,67 +171,68 @@ function MovieDetail({ match }) {
           ) : (
             <div>
               <hr></hr>
-              <h4>No Movies Found Yet</h4>
+              <h4>Collecting, please wait</h4>
             </div>)
           }
+
           <hr></hr>
           <div className="movie__overview">
-        <div className="small_poster">
-            <img 
-            key={movie.id}
-            className="poster__overview"
-            src={`${base_url}${movie.poster_path}`}
-            alt={movie.name}
-            />
-        </div>
-        <div className="movie_story">
-            <h1>Storyline</h1>
-            <h1>{movie.overview}</h1>
-       
-        <div className="movie_information">
-          <div className="movie_info">
-            <h1>Released </h1>
-            <h1>Budget </h1>
-            <h1>Revenue </h1>
-            <h1>Status</h1>
-            <h1>Runtime</h1>
-            <h1>Genre </h1>
-          </div>
-          <div className="movie_api_info">
-            <h1>{movie.release_date}</h1>
-            <h1>${movie.budget}</h1>
-            <h1>${movie.revenue}</h1>
-            <h1>{movie.status}</h1>
-            <h1>{Math.floor(movie.runtime / 60)}h {movie.runtime % 60}min</h1>
-            {/* <h1>{movie.genres[0].name}, {movie.genres[1].name}</h1> */}
-          </div>
-        </div>
-        </div>
-      </div>
-
-      <div className="cast-details">
-        <div className="cast">
-        <h1>Cast</h1>
-        </div>
-        {credits.slice(0, 10).map((credit) => (
-          <Link to={`/person/${credit.id}`}  >
-          <div className="row__cast">
-            <div className="img__cast">
-            
+            <div className="small_poster">
               <img
-                // onClick={() => handleClick(movie)}
-                className="img__actor"
-                src={`${base_url}${credit.profile_path}`}
-                alt={credit.name}
+                key={movie.id}
+                className="poster__overview"
+                src={`${base_url}${movie.poster_path}`}
+                alt={movie.name}
               />
             </div>
-            <h1>{credit.name}</h1>
-           
-                        
+            <div className="movie_story">
+              <h1>Storyline</h1>
+              <h1>{movie.overview}</h1>
+
+              <div className="movie_information">
+                <div className="movie_info">
+                  <h1>Released </h1>
+                  <h1>Budget </h1>
+                  <h1>Revenue </h1>
+                  <h1>Status</h1>
+                  <h1>Runtime</h1>
+                  <h1>Genre </h1>
+                </div>
+                <div className="movie_api_info">
+                  <h1>{movie.release_date}</h1>
+                  <h1>${movie.budget}</h1>
+                  <h1>${movie.revenue}</h1>
+                  <h1>{movie.status}</h1>
+                  <h1>{Math.floor(movie.runtime / 60)}h {movie.runtime % 60}min</h1>
+                  {/* <h1>{movie.genres[0].name}, {movie.genres[1].name}</h1> */}
+                </div>
+              </div>
+            </div>
           </div>
-          </Link>
-        ))}
-      </div>
+
+          <div className="cast-details">
+            <div className="cast">
+              <h1>Cast</h1>
+            </div>
+            {credits.slice(0, 10).map((credit) => (
+              <Link to={`/person/${credit.id}`}  >
+                <div className="row__cast">
+                  <div className="img__cast">
+
+                    <img
+                      // onClick={() => handleClick(movie)}
+                      className="img__actor"
+                      src={`${base_url}${credit.profile_path}`}
+                      alt={credit.name}
+                    />
+                  </div>
+                  <h1>{credit.name}</h1>
+
+
+                </div>
+              </Link>
+            ))}
+          </div>
 
         </div>
         <div>
@@ -243,33 +255,32 @@ function MovieDetail({ match }) {
 
       <div className="details__fadeBottom"></div>
       <div className="more_like_this">
-            <div className="row">
-              <div className="row__posters" >
-              <h1>More Like this</h1>
-                {similars.map((similar) => (
-                  <Link to={`/movie/${similar.id}`}  >
-                    <div className="poster" >
-                      <img
-                        key={similar.id}
-                        // onClick={() => handleClick(movie)}
-                        className={`row__poster ${ "row__posterLarge"}`}
-                        src={`${base_url}${
-                          similar.poster_path || similar.backdrop_path
-                        }`}
-                        alt={similar.name}
-                      />
-                      <div className="onhover" >
-                        <h1>{similar.title}</h1>
-                        <h1>{similar.overview}</h1>
-                        <h1>{similar.media_type}</h1>
-                      </div>
-                    </div>
-                  </Link>
-                )
-                )}
-              </div>
+        <div className="row">
+          <div className="row__posters" >
+            <h1>More Like this</h1>
+            {similars.map((similar) => (
+              <Link to={`/movie/${similar.id}`}  >
+                <div className="poster" >
+                  <img
+                    key={similar.id}
+                    // onClick={() => handleClick(movie)}
+                    className={`row__poster ${"row__posterLarge"}`}
+                    src={`${base_url}${similar.poster_path || similar.backdrop_path
+                      }`}
+                    alt={similar.name}
+                  />
+                  <div className="onhover" >
+                    <h1>{similar.title}</h1>
+                    <h1>{similar.overview}</h1>
+                    <h1>{similar.media_type}</h1>
+                  </div>
+                </div>
+              </Link>
+            )
+            )}
           </div>
         </div>
+      </div>
 
     </div>
   );
